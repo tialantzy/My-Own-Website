8a98ad118d09317668cd7ff3b06b2afdc13881c4
tialantzy <tialantzy@gmail.com>
2022-06-20 09:59:19 -0500

    practice commit

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
 const h2 = document.createElement("h2");
-h2.textContent = "This content added by JavaScript";
+h2.textContent = "How Do I Learn This?";
 document.querySelector("body").appendChild(h2);
+
